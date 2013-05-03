cee34143b9e629ea98fa653c1b1b36a00af28b58
bergh <bergh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-03 22:41:43 +0000

    0.3.1 in tags
    git-svn-id: https://plugins.svn.wordpress.org/link-icons/tags/0.3.1@707673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ link_icons_functions.php
@@ -0,0 +1,93 @@
+<?php
+function link_icons($content)
+{
+	if(strlen($content)>1)
+	{
+		//error_log($content, 0);
+		$D = new DOMDocument;
+		$content=mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8"); 
+		$D->loadHTML($content);
+		processElement($D);
+		return $D->saveHTML();
+	}
+}	
+
+function processElement(DOMNode $element){
+    foreach($element->childNodes as $child){
+		if($child instanceOf DOMElement){
+           switch($child->nodeName){
+            case 'a':
+				if($child->textContent!=""){
+					foreach ($child->attributes as $attr) {
+						$name = $attr->nodeName;
+						$value = $attr->nodeValue;	
+						if($name=="href")
+						{
+							linkType($value,$child);
+						}
+					}
+				}
+                break;           
+            default:
+                processElement($child);
+            }
+		}
+    }
+}
+
+function linkType($url,$child){
+	
+	$linkURL = $url;
+	$header=get_headers($url,1);	
+	
+	$contentType=$header['Content-Type'];
+	
+	if(is_array($contentType)) $contentType = $contentType[0];
+	
+	// is the link a directlink to an image?
+	// I could here just check if the url ends in jpg/jpeg/gif/png, but what if it is link to a php file with imaagick? 
+	// so we gonna check the mimetipe.
+	
+	if(substr($contentType,0,5)=="image")
+	{
+		$child->setAttribute("class", "link-icons-image");	
+	}
+	
+	// is the link a directlink to an video?
+	// we gonna check the mimetipe.
+	elseif(substr($contentType,0,5)=="video" || preg_match('/\youtube\.com\/watch.*/',$url))
+	{
+		$child->setAttribute("class", "link-icons-video");	
+	}
+	
+	// checking if it is a internal or external link
+	elseif(substr($linkURL,0,7)=="http://" || substr($linkURL,0,8)=="https://")
+	{
+		$linkURL=substr($linkURL,7,strlen($linkURL)-7);	
+		
+		//getting just the host/domain
+		$linkURL=explode("/",$linkURL);
+		$linkURL=$linkURL[0];
+		
+		if($linkURL!=$_SERVER['HTTP_HOST'])
+		{
+			$child->setAttribute("class", "link-icons-external");
+		}
+		else
+		{
+			$child->setAttribute("class", "link-icons-internal");
+		}
+	}
+	else // if the href does not have http:// it's internal
+	{
+		$child->setAttribute("class", "link-icons-internal");
+	}
+	
+	
+	
+	
+	
+	
+	
+}	
+?>
---
+++ readme.txt
@@ -0,0 +1,90 @@
+=== Link Icons ===
+Contributors: bergh
+Donate link: 
+Tags:  Link, Icons, External link, Image link, Video link
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: 0.3.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Link Icons adds an icon next you your different types of links in your posts an pages
+
+== Description ==
+
+Link Icons adds a icon next you your different types of links in your posts an pages.
+
+Link Icons does not connect to any external sites.
+
+Since Link Icons uses Http Header to check content type, using a caching plugin for your wordpress site is recomended.
+
+
+= 0.3.1 =
+
+*	Fix for this http://wordpress.org/support/topic/i-will-come-back-later
+*	Fix for https-links
+
+= 0.3 =
+
+*	Video link type
+*	Sets icons to links to URLs that return Video content type.
+*	Sets icons to links to a youtube video.
+
+= 0.2 =
+
+*	Ignores a-tags if it wraps an img.
+*	Sets icons to links to URLs that return Image content type
+
+= 0.1 =
+
+*	Initial upload
+*	External links
+
+
+== Installation ==
+
+= Easiest installation =
+
+1. In wordpress admin go to Plugins -> Add plugin
+2. Search for "Link Icons" and add
+
+
+= Alternative installation =
+
+1. Upload the `link-icons` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently asked questions ==
+
+= Can I set my own icons? =
+
+Not yet, but soon.
+
+
+= What type of links does it add icons to? =
+
+-	External links
+-	Links to images
+-	Links to videos
+-	Links to youtubevideos
+
+More to come
+
+= Can I turn of one of the link types? =
+
+Not yet, but by the end of this week an admin interface will give you this opportunity
+
+= Can I turn of use of HTTP Header? =
+
+Not yet, but by the end of this week an admin interface will give you this opportunity
+
+
+== Screenshots ==
+
+1. This is how it looks
+
+== Upgrade Notice ==
+
+*	Fix for this http://wordpress.org/support/topic/i-will-come-back-later
+*	Fix for https-links
